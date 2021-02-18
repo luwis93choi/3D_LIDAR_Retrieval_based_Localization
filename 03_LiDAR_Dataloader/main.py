2a7661b4bc096e450ccd9abb522464b72501e89c
luwis93choi <luwis93choi@hotmail.com>
2021-02-19 05:38:05 +0900

    LiDAR, Image, Pose Dataloader

--- 03_LiDAR_Dataloader/main.py
+++ 03_LiDAR_Dataloader/main.py
@@ -1,9 +1,13 @@
 import os
 import argparse
 import cv2 as cv
+import numpy as np
 import open3d as o3d
+from torch import threshold
 
 from sensor_dataloader import sensor_dataloader
+
+import torchvision.transforms.functional as TF
 
 ap = argparse.ArgumentParser()
 
@@ -13,7 +17,22 @@ ap.add_argument('-p', '--input_pose_file_path', type=str, required=True)
 
 args = vars(ap.parse_args())
 
-dataset_gen = sensor_dataloader(lidar_dataset_path=args['input_lidar_file_path'], 
+dataloader = sensor_dataloader(lidar_dataset_path=args['input_lidar_file_path'], 
                                 img_dataset_path=args['input_img_file_path'], 
                                 pose_dataset_path=args['input_pose_file_path'],
                                 train_sequence=['00', '01', '02'], valid_sequence=['01'], test_sequence=['02'])
+
+for batch_idx, (lidar_range_img_tensor, current_img_tensor, pose_6DOF_tensor) in enumerate(dataloader):
+
+    lidar_range_img = np.array(TF.to_pil_image(lidar_range_img_tensor))
+    lidar_range_img = cv.resize(lidar_range_img, dsize=(1280, 240), interpolation=cv.INTER_CUBIC)
+    lidar_range_img = cv.applyColorMap(lidar_range_img, cv.COLORMAP_HSV)
+
+    current_img = np.array(TF.to_pil_image(current_img_tensor))
+    current_img = cv.cvtColor(current_img, cv.COLOR_RGB2BGR)
+    current_img = cv.resize(current_img, dsize=(1280, 240), interpolation=cv.INTER_CUBIC)
+
+    img_total = cv.vconcat([lidar_range_img, current_img])
+
+    cv.imshow('3D LiDAR Range Image', img_total)
+    cv.waitKey(1)
--- 03_LiDAR_Dataloader/sensor_dataloader.py
+++ 03_LiDAR_Dataloader/sensor_dataloader.py
@@ -5,6 +5,12 @@ import csv
 
 import torch
 import torch.utils.data
+import torchvision.transforms.functional as TF
+
+import PIL
+from PIL import Image
+
+from lidar_range_img_generator import range_img_generator
 
 class dataset_dict_generator():
 
@@ -17,23 +23,31 @@ class dataset_dict_generator():
 
 
         dataset_sequence_list = np.array([train_sequence, valid_sequence, test_sequence], dtype=np.object)
+        
+        self.train_dataset_dict_path = './Train_lidar_dataset.csv'
+        self.valid_dataset_dict_path = './Valid_lidar_dataset.csv'
+        self.test_dataset_dict_path = './Test_lidar_dataset.csv'
 
-        ############################################
+        self.train_len = 0
+        self.valid_len = 0
+        self.test_len = 0
+
+        ######################################
         ### Dataset Dictionary Preparation ###
-        ############################################
+        ######################################
         dataset_dict_idx = 0
         for dataset_type in dataset_sequence_list:
 
             self.data_idx = 0
 
             if dataset_dict_idx == 0:
-                self.dataset_dict = open('./Train_lidar_dataset.csv', 'w', encoding='utf-8', newline='')
+                self.dataset_dict = open(self.train_dataset_dict_path, 'w', encoding='utf-8', newline='')
 
             elif dataset_dict_idx == 1:
-                self.dataset_dict = open('./Valid_lidar_dataset.csv', 'w', encoding='utf-8', newline='')
+                self.dataset_dict = open(self.valid_dataset_dict_path, 'w', encoding='utf-8', newline='')
 
             elif dataset_dict_idx == 2:
-                self.dataset_dict = open('./Test_lidar_dataset.csv', 'w', encoding='utf-8', newline='')
+                self.dataset_dict = open(self.test_dataset_dict_path, 'w', encoding='utf-8', newline='')
 
             self.dataset_writer = csv.writer(self.dataset_dict)
 
@@ -84,6 +98,15 @@ class dataset_dict_generator():
 
                     self.data_idx += 1
 
+            if dataset_dict_idx == 0:
+                self.train_len = self.data_idx
+
+            elif dataset_dict_idx == 1:
+                self.valid_len = self.valid_len
+
+            elif dataset_dict_idx == 2:
+                self.test_len = self.test_len
+
             self.dataset_dict.close()
 
             dataset_dict_idx += 1
@@ -116,7 +139,67 @@ class sensor_dataloader(torch.utils.data.Dataset):
 
         self.mode = mode
 
+        self.lidar_range_img_generator = range_img_generator(h_fov=[-180, 180], h_res=0.2, v_fov=[-24.9, 2], v_res=0.4)
+
         self.dataset_dict_generator = dataset_dict_generator(lidar_dataset_path=lidar_dataset_path, 
                                                              img_dataset_path=img_dataset_path, 
                                                              pose_dataset_path=pose_dataset_path,
                                                              train_sequence=train_sequence, valid_sequence=valid_sequence, test_sequence=test_sequence)
+
+        self.train_data_list = []
+        train_dataset_dict = open(self.dataset_dict_generator.train_dataset_dict_path, 'r', encoding='utf-8')
+        train_reader = csv.reader(train_dataset_dict)
+        next(train_reader)     # Skip header row
+        for row_data in train_reader:
+            self.train_data_list.append(row_data)
+        train_dataset_dict.close()
+
+        self.valid_data_list = []
+        valid_dataset_dict = open(self.dataset_dict_generator.valid_dataset_dict_path, 'r', encoding='utf-8')
+        valid_reader = csv.reader(valid_dataset_dict)
+        next(valid_reader)     # Skip heaer row
+        for row_data in valid_reader:
+            self.valid_data_list.append(row_data)
+        valid_dataset_dict.close()
+
+        self.test_data_list = []
+        test_dataset_dict = open(self.dataset_dict_generator.test_dataset_dict_path, 'r', encoding='utf-8')
+        test_reader = csv.reader(test_dataset_dict)
+        next(test_reader)      # Skip header row
+        for row_data in test_reader:
+            self.test_data_list.append(row_data)
+        test_dataset_dict.close()
+
+    def __getitem__(self, index):
+
+        if self.mode == 'training':
+            item = self.train_data_list[index]
+
+        elif self.mode == 'validation':
+            item = self.valid_data_list[index]
+
+        elif self.mode == 'test':
+            item = self.test_data_list[index]
+        
+        lidar_range_img = self.lidar_range_img_generator.convert_range_img(pcd_path=item[2], output_type='img_pixel')
+        lidar_range_img = TF.to_tensor(lidar_range_img)
+
+        current_img = Image.open(item[3])
+        current_img = TF.to_tensor(current_img)
+
+        pose_6DOF = [float(i) for i in item[4:]]
+        pose_6DOF = torch.tensor(pose_6DOF)
+
+        return lidar_range_img, current_img, pose_6DOF
+
+    def __len__(self):
+
+        if self.mode == 'training':
+            return self.dataset_dict_generator.train_len
+
+        elif self.mode == 'validation':
+            return self.dataset_dict_generator.valid_len
+
+        elif self.mode == 'test':
+            return self.dataset_dict_generator.test_len
+
