06e2f9879f4931720215258a166e64885543adc5
luwis93choi <luwis93choi@hotmail.com>
2021-03-12 07:42:35 +0900

    Added SSIM Loss and layer display function

--- 05_CNN_Autoencoder/CNN_Autoencoder.py
+++ 05_CNN_Autoencoder/CNN_Autoencoder.py
@@ -7,6 +7,12 @@ import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
 
+import cv2 as cv
+
+import math
+
+import pytorch_ssim     # SSIM Loss : https://github.com/Po-Hsun-Su/pytorch-ssim
+
 class CNN_Autoencoder(nn.Module):
 
     def __init__(self, device=None, input_size=[1, 3, 100, 100], batch_size=1, learning_rate=0.001):
@@ -51,14 +57,7 @@ class CNN_Autoencoder(nn.Module):
         self.batchNorm4 = nn.BatchNorm2d(256)
         self.leakyrelu4 = nn.LeakyReLU(0.1)
 
-        self.flatten_encoder = nn.Flatten(start_dim=1)
-        
-        self.encoder_linear = nn.Linear(in_features=256 * 15 * 80, out_features=2)
-
-
         ### Decoder ###
-        self.decoder_linear = nn.Linear(in_features=2, out_features=256 * 15 * 80)
-
         self.deconv1 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), output_padding=(1, 1), bias=False)
         self.batchNorm_deconv1 = nn.BatchNorm2d(128)
         self.leakyrelu_deconv1 = nn.LeakyReLU(0.1)
@@ -71,17 +70,61 @@ class CNN_Autoencoder(nn.Module):
         self.batchNorm_deconv3 = nn.BatchNorm2d(32)
         self.leakyrelu_deconv3 = nn.LeakyReLU(0.1)
 
-        self.deconv4 = nn.ConvTranspose2d(in_channels=32, out_channels=6, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), output_padding=(1, 1), bias=False)
+        self.deconv4 = nn.ConvTranspose2d(in_channels=32, out_channels=input_size[1], kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), output_padding=(1, 1), bias=False)
         
         self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
 
-        self.loss = nn.MSELoss()
+        # self.loss = nn.MSELoss()
+        self.loss = pytorch_ssim.SSIM(window_size=11)
 
         self.batch_size = batch_size
 
         self.to(device)
 
+    # CNN Layer Result Display Function - Display 2D Convolution Results by Channels
+    def layer_disp(self, conv_result_x, window_name, col_num, resize_ratio=0.8, invert=False):
+
+        x_disp = conv_result_x.clone().detach().cpu()
+
+        if invert == True:
+            img_stack = 255 - (x_disp.permute(0, 2, 3, 1)[0, :, :, :].numpy()*255).astype(np.uint8)
+        else:
+            img_stack = (x_disp.permute(0, 2, 3, 1)[0, :, :, :].numpy()*255).astype(np.uint8)
+
+        channel = img_stack.shape[2]
+
+        cols = col_num
+        rows = int(math.ceil(channel/cols))
+
+        resize_ratio = resize_ratio
+
+        for i in range(rows):
+            
+            for j in range(cols):
+                
+                if (j + cols*i) >= channel:
+                    blank = np.zeros((width, height, 1), np.uint8)
+                    img_horizontal = cv.hconcat([img_horizontal, blank])
+
+                elif j == 0:
+                    img_horizontal = cv.resize(img_stack[:, :, j + cols*i], dsize=(0, 0), fx=resize_ratio, fy=resize_ratio, interpolation=cv.INTER_LINEAR)
+                    width, height = img_horizontal.shape
+
+                else:
+                    input_img = cv.resize(img_stack[:, :, j + cols*i], dsize=(0, 0), fx=resize_ratio, fy=resize_ratio, interpolation=cv.INTER_LINEAR)
+                    img_horizontal = cv.hconcat([img_horizontal, input_img])
+
+            if i == 0:
+                img_total = img_horizontal
+            else:
+                img_total = cv.vconcat([img_total, img_horizontal])
+
+        cv.imshow(window_name, img_total)
+        cv.waitKey(1)
+
     def forward(self, x):
+
+        self.layer_disp(x, window_name='Input Img', col_num=3, resize_ratio=0.4)
 
         # print('----------------------------------------')
         # print(x.shape)
@@ -110,22 +153,6 @@ class CNN_Autoencoder(nn.Module):
 
         # print(x.shape)
 
-        x = self.flatten_encoder(x)
-
-        # print(x.shape)
-
-        x = self.encoder_linear(x)
-
-        # print(x.shape)
-
-        x = self.decoder_linear(x)
-
-        # print(x.shape)
-
-        x = x.view(-1, 256, 15, 80)
-
-        # print(x.shape)
-
         x = self.deconv1(x)
         x = self.batchNorm_deconv1(x)
         x = self.leakyrelu_deconv1(x)
@@ -147,5 +174,7 @@ class CNN_Autoencoder(nn.Module):
         x = self.deconv4(x)
 
         # print(x.shape)
+
+        self.layer_disp(x, window_name='Recovered Img', col_num=3, resize_ratio=0.4)
 
         return x
