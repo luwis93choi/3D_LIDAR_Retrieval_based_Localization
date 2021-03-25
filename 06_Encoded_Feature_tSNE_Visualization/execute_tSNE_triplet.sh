516f18bd1dd46c3aad6cbd8b172ce3aec1dfc3ef
luwis93choi <luwis93choi@hotmail.com>
2021-03-25 14:52:51 +0900

    Added mode selection between training and visualization

--- 06_Encoded_Feature_tSNE_Visualization/execute_tSNE_triplet.sh
+++ 06_Encoded_Feature_tSNE_Visualization/execute_tSNE_triplet.sh
@@ -2,15 +2,22 @@
 
 image_dataset_path="/media/luwis/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/dataset/sequences"
 pose_dataset_path="/media/luwis/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/data_odometry_poses/dataset/poses"
+pretrained_model_path="/"
 
-batch_size=4
+# 'training'
+# 'tsne'
+mode='tsne' 
+
+batch_size=1
 
 cuda_num='0'
 
 training_epoch=100
 
-python3 Agglo_tSNE_visualization.py --input_img_file_path "$image_dataset_path" \
+python3 Agglo_tSNE_visualization.py --mode "$mode" \
+                                    --input_img_file_path "$image_dataset_path" \
                                     --input_pose_file_path "$pose_dataset_path" \
+                                    --pretrained_model_path "$pretrained_model_path" \
                                     --input_batch_size "$batch_size" \
                                     --input_CUDA_num "$cuda_num" \
                                     --training_epoch "$training_epoch" \
