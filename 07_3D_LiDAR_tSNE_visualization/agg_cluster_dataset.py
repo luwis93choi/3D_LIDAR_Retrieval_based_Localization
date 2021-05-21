f9ba832d9f961ea4c8b032c23104545dfe4aa952
luwis <luwis93choi@hotmail.com>
2021-05-21 01:34:33 +0000

    Init commit for lidar based feature clustering

---
+++ 07_3D_LiDAR_tSNE_visualization/agg_cluster_dataset.py
@@ -0,0 +1,348 @@
+import os
+import os.path
+import numpy as np
+import csv
+
+import torch
+import torch.utils.data
+from torchvision import transforms
+import torchvision.transforms.functional as TF
+
+import PIL
+from PIL import Image
+
+import cv2 as cv
+
+import random
+import copy
+
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.model_selection import train_test_split
+from torchvision.transforms.transforms import RandomErasing, ToTensor
+
+from lidar_range_img_generator import range_img_generator
+
+class dataset_dict_generator():
+
+    def __init__(self, lidar_dataset_path='', pose_dataset_path='',
+                       sequence_to_use=['00'], train_ratio=0, valid_ratio=0, test_ratio=0,
+                       cluster_linkage='ward', cluster_distance=1.0):
+
+        self.lidar_dataset_path = lidar_dataset_path
+        self.pose_dataset_path = pose_dataset_path
+        
+        self.full_dataset_dict_path = './[Cluster]Full_lidar_dataset.csv'
+
+        self.train_dataset_dict_path = './[Cluster]Train_lidar_dataset.csv'
+        self.valid_dataset_dict_path = './[Cluster]Valid_lidar_dataset.csv'
+        self.test_dataset_dict_path = './[Cluster]Test_lidar_dataset.csv'
+
+        self.train_len = 0
+        self.valid_len = 0
+        self.test_len = 0
+
+        self.agglo_clusterer = AgglomerativeClustering(n_clusters=None, linkage=cluster_linkage, distance_threshold=cluster_distance)
+
+        self.seq_path_dict = {}
+
+        ######################################
+        ### Dataset Dictionary Preparation ###
+        ######################################
+        
+        self.full_dataset_dict = open(self.full_dataset_dict_path, 'w', encoding='utf-8', newline='')
+        self.train_dataset_dict = open(self.train_dataset_dict_path, 'w', encoding='utf-8', newline='')
+        self.valid_dataset_dict = open(self.valid_dataset_dict_path, 'w', encoding='utf-8', newline='')
+        self.test_dataset_dict = open(self.test_dataset_dict_path, 'w', encoding='utf-8', newline='')
+
+        self.full_dataset_writer = csv.writer(self.full_dataset_dict)
+        self.train_dataset_writer = csv.writer(self.train_dataset_dict)
+        self.valid_dataset_writer = csv.writer(self.valid_dataset_dict)
+        self.test_dataset_writer = csv.writer(self.test_dataset_dict)
+
+        header_list = ['current_index', 'Sequence_num', 'Sequence_idx', 'current_img_path', 'current_x [m]', 'current_y [m]', 'current_z [m]', 'current_roll [rad]', 'current_pitch [rad]', 'current_yaw [rad]', 'cluster_label', 'type']
+        self.full_dataset_writer.writerow(header_list)
+        self.train_dataset_writer.writerow(header_list)
+        self.valid_dataset_writer.writerow(header_list)
+        self.test_dataset_writer.writerow(header_list)
+
+        ### Iteration over all dataset sequences ###
+        self.data_idx = 0
+        for sequence_num in np.array(sequence_to_use):
+
+            ### LiDAR data path accumulation ###
+            lidar_base_path = self.lidar_dataset_path + '/' + sequence_num + '/velodyne'
+            lidar_data_name = np.array(sorted(os.listdir(lidar_base_path)))
+
+            ### Pose data accumulation ###
+            lines = []
+            pose_file = open(self.pose_dataset_path + '/' + sequence_num + '.txt', 'r')
+            while True:
+                line = pose_file.readline()
+                
+                if not line: break
+                
+                lines.append(line)
+                
+            pose_file.close()
+
+            self.sequence_idx = 0
+            pose_list = []
+
+            ### Labeling using Agglomerative Clustering ###
+            print('Agglomerative Clustering-based Labeling : Sequence : {}'.format(sequence_num))
+            for line in np.array(lines):
+                
+                # Pose data re-organization into x, y, z, euler angles
+                pose_line = line
+                pose = pose_line.strip().split()
+                
+                current_pose_T = [float(pose[3]), float(pose[7]), float(pose[11])]
+                current_pose_Rmat = np.array([
+                                            [float(pose[0]), float(pose[1]), float(pose[2])],
+                                            [float(pose[4]), float(pose[5]), float(pose[6])],
+                                            [float(pose[8]), float(pose[9]), float(pose[10])]
+                                            ])
+
+                current_x = current_pose_T[0]
+                current_y = current_pose_T[1]
+                current_z = current_pose_T[2]
+
+                current_roll = np.arctan2(current_pose_Rmat[2][1], current_pose_Rmat[2][2])
+                current_pitch = np.arctan2(-1 * current_pose_Rmat[2][0], np.sqrt(current_pose_Rmat[2][1]**2 + current_pose_Rmat[2][2]**2))
+                current_yaw = np.arctan2(current_pose_Rmat[1][0], current_pose_Rmat[0][0])
+
+                pose_list.append(np.array([current_x, current_y, current_z, current_roll, current_pitch, current_yaw]))
+
+            pose_list = np.array(pose_list)
+            xyz_pose_list = pose_list[:, 0:3]
+
+            clusters = self.agglo_clusterer.fit(xyz_pose_list)
+
+            ### Train, Valid, Test split dataset on each sequence ###
+            cluster_labels = np.array(clusters.labels_)
+            data_type_list = np.array(['train'] * len(cluster_labels))
+            for label in np.unique(cluster_labels):
+
+                mask = (cluster_labels == label)
+
+                label_idx = np.where(mask)
+
+                train_idx = []
+                valid_idx = []
+                test_idx = []
+                valid_test_idx = []
+
+                if len(label_idx[0]) > 1:
+                    # Split between train and validation/test
+
+                    train_idx, valid_test_idx = train_test_split(label_idx[0], test_size=test_ratio, random_state=42, shuffle=True)
+
+                    data_type_list[train_idx] = 'train'
+
+                    ### Create positive image path list for each cluster label ###
+                    base_path_list = np.array([lidar_base_path] * len(train_idx))
+                    path_list_append = np.array(['/'] * len(train_idx))
+                    combined_path_list = np.core.defchararray.add(base_path_list, path_list_append)
+                    combined_path_list = np.core.defchararray.add(combined_path_list, lidar_data_name[train_idx])
+                    self.seq_path_dict['{}-{}'.format(sequence_num, label)] = combined_path_list
+
+                else:
+                    # Exception Handling (Only 1 image in cluster) : Randomly assign the leftover as validation or test
+                    data_type_list[label_idx] = 'train'
+
+                    ### Create positive image path list for each cluster label ###
+                    base_path_list = np.array([lidar_base_path] * len(label_idx))
+                    path_list_append = np.array(['/'] * len(label_idx))
+                    combined_path_list = np.core.defchararray.add(base_path_list, path_list_append)
+                    combined_path_list = np.core.defchararray.add(combined_path_list, lidar_data_name[label_idx])
+                    self.seq_path_dict['{}-{}'.format(sequence_num, label)] = combined_path_list
+
+                if len(valid_test_idx) > 1:
+                    # Split between validation and test
+                    valid_test_ratio = valid_ratio + test_ratio
+                    rearranged_test_ratio = 1 - (valid_ratio / valid_test_ratio)
+
+                    valid_idx, test_idx = train_test_split(valid_test_idx, test_size=rearranged_test_ratio, random_state=42, shuffle=True)
+
+                    data_type_list[valid_idx] = 'valid'
+
+                    data_type_list[test_idx] = 'test'
+
+                else:
+                    # Exception Handling (Only 1 image to split between validation and test) : Randomly assign the leftover as validation or test
+                    for idx in valid_test_idx:
+
+                        if random.random() >= 0.5:
+                            data_type_list[idx] = 'valid'
+                        else:
+                            data_type_list[idx] = 'test'
+
+            ### Writing dataset attributes on Full Dataset csv ###
+            for img_name, pose, cluster_label, data_type in zip(lidar_data_name, pose_list, cluster_labels, data_type_list):
+                
+                data = [self.data_idx, sequence_num, self.sequence_idx, \
+                        lidar_base_path + '/' + img_name, \
+                        pose[0], pose[1], pose[2], pose[3], pose[4], pose[5], \
+                        '{}-{}'.format(sequence_num, cluster_label), \
+                        data_type]
+
+                self.full_dataset_writer.writerow(data)
+
+                if data_type == 'train':
+                    self.train_dataset_writer.writerow(data)
+                
+                elif data_type == 'valid':
+                    self.valid_dataset_writer.writerow(data)
+
+                elif data_type == 'test':
+                    self.test_dataset_writer.writerow(data)
+
+                self.data_idx += 1
+                self.sequence_idx += 1
+
+        self.full_dataset_dict.close()
+        self.train_dataset_dict.close()
+        self.valid_dataset_dict.close()
+        self.test_dataset_dict.close()
+
+class sensor_dataset(torch.utils.data.Dataset):
+
+    def __init__(self, lidar_dataset_path='', pose_dataset_path='',
+                       sequence_to_use=['00'], train_ratio=0, valid_ratio=0, test_ratio=0,
+                       cluster_linkage='ward', cluster_distance=1.0, 
+                       mode='training', output_resolution=[1280, 240],
+                       transform=None):
+
+        self.mode = mode
+        self.output_resolution = output_resolution
+        self.transform = transform
+
+        if self.transform is None:
+            self.transform = transforms.Compose([transforms.Resize((output_resolution[1], output_resolution[0])),
+                                                 transforms.RandomApply([transforms.ColorJitter(brightness=0.5, contrast=0.5)], p=0.5),
+                                                 transforms.ToTensor(),
+                                                 transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 0.3)),])
+        else:
+            self.transform = transform
+
+        self.lidar_range_img_generator = range_img_generator(h_fov=[-180, 180], h_res=0.2, v_fov=[-24.9, 2], v_res=0.4)
+
+        self.dataset_dict_generator = dataset_dict_generator(lidar_dataset_path=lidar_dataset_path, pose_dataset_path=pose_dataset_path,
+                                                             sequence_to_use=sequence_to_use, train_ratio=train_ratio, valid_ratio=valid_ratio, test_ratio=test_ratio,
+                                                             cluster_linkage=cluster_linkage, cluster_distance=cluster_distance)
+
+        self.train_data_list = []
+        train_dataset_dict = open(self.dataset_dict_generator.train_dataset_dict_path, 'r', encoding='utf-8')
+        train_reader = csv.reader(train_dataset_dict)
+        next(train_reader)     # Skip header row
+        for row_data in train_reader:
+            self.train_data_list.append(row_data)
+        self.dataset_dict_generator.train_len = len(self.train_data_list)
+        print('Training Length : {}'.format(self.dataset_dict_generator.train_len))
+        train_dataset_dict.close()
+
+        self.valid_data_list = []
+        valid_dataset_dict = open(self.dataset_dict_generator.valid_dataset_dict_path, 'r', encoding='utf-8')
+        valid_reader = csv.reader(valid_dataset_dict)
+        next(valid_reader)     # Skip heaer row
+        for row_data in valid_reader:
+            self.valid_data_list.append(row_data)
+        self.dataset_dict_generator.valid_len = len(self.valid_data_list)
+        print('Validation Length : {}'.format(self.dataset_dict_generator.valid_len))
+        valid_dataset_dict.close()
+
+        self.test_data_list = []
+        test_dataset_dict = open(self.dataset_dict_generator.test_dataset_dict_path, 'r', encoding='utf-8')
+        test_reader = csv.reader(test_dataset_dict)
+        next(test_reader)      # Skip header row
+        for row_data in test_reader:
+            self.test_data_list.append(row_data)
+        self.dataset_dict_generator.test_len = len(self.test_data_list)
+        print('Test Length : {}'.format(self.dataset_dict_generator.test_len))
+        test_dataset_dict.close()
+
+    def __getitem__(self, index):
+
+        if self.mode == 'training':
+            item = self.train_data_list[index]
+
+        elif self.mode == 'validation':
+            item = self.valid_data_list[index]
+
+        elif self.mode == 'test':
+            item = self.test_data_list[index]
+
+        ##############################
+        ### Anchor Image Selection ###
+        ##############################
+
+        anchor_img = self.lidar_range_img_generator.convert_range_img(pcd_path=item[3], output_type='img_pixel')
+        anchor_img = np.squeeze(anchor_img, axis=0)
+        anchor_img = cv.resize(anchor_img, dsize=(self.output_resolution[0], self.output_resolution[1]), interpolation=cv.INTER_CUBIC)
+        
+        anchor_img = Image.fromarray(anchor_img.astype(np.uint8)).convert('RGB')
+        anchor_img = TF.to_tensor(anchor_img)
+        
+
+        ################################
+        ### Positive Image Selection ###
+        ################################
+
+        anchor_img_label = item[10]
+
+        positive_path_list = self.dataset_dict_generator.seq_path_dict[anchor_img_label]
+        
+        ### Exception Handling ###
+        # If there are not enough images in the cluster, apply user-defined data augmentation.
+        # Use augmented image as positive data
+        if len(positive_path_list) <= 1:
+            
+            positive_img = self.lidar_range_img_generator.convert_range_img(item[3], output_type='img_pixel')
+            positive_img = np.squeeze(positive_img, axis=0)
+            positive_img = Image.fromarray(positive_img.astype(np.uint8)).convert('RGB')
+            positive_img = self.transform(positive_img)
+
+        else:
+            
+            positive_img_path = random.choice(positive_path_list)
+
+            positive_img = self.lidar_range_img_generator.convert_range_img(positive_img_path, output_type='img_pixel')
+            positive_img = np.squeeze(positive_img, axis=0)
+            positive_img = cv.resize(positive_img, dsize=(self.output_resolution[0], self.output_resolution[1]), interpolation=cv.INTER_CUBIC)
+            
+            positive_img = Image.fromarray(positive_img.astype(np.uint8)).convert('RGB')
+            positive_img = TF.to_tensor(positive_img)
+
+        ################################
+        ### Negative Image Selection ###
+        ################################
+
+        negative_labels = list(self.dataset_dict_generator.seq_path_dict.keys())
+        negative_labels.remove(anchor_img_label)
+
+        negative_label_choice = random.choice(negative_labels)
+
+        negative_path_list = self.dataset_dict_generator.seq_path_dict[negative_label_choice]
+        
+        negative_img_path = random.choice(negative_path_list)
+        
+        negative_img = self.lidar_range_img_generator.convert_range_img(pcd_path=negative_img_path, output_type='img_pixel')
+        negative_img = np.squeeze(negative_img, axis=0)
+        negative_img = cv.resize(negative_img, dsize=(self.output_resolution[0], self.output_resolution[1]), interpolation=cv.INTER_CUBIC)
+        
+        negative_img = Image.fromarray(negative_img.astype(np.uint8)).convert('RGB')
+        negative_img = TF.to_tensor(negative_img)
+        
+        return anchor_img, positive_img, negative_img, anchor_img_label
+
+    def __len__(self):
+
+        if self.mode == 'training':
+            return self.dataset_dict_generator.train_len
+
+        elif self.mode == 'validation':
+            return self.dataset_dict_generator.valid_len
+
+        elif self.mode == 'test':
+            return self.dataset_dict_generator.test_len
+
---
+++ 07_3D_LiDAR_tSNE_visualization/lidar_range_img_generator.py
@@ -0,0 +1,103 @@
+# Reference 01 : KITTI Tutorial (https://github.com/windowsub0406/KITTI_Tutorial)
+# Reference 02 : Vehicle Detection from 3D Lidar Using Fully Convolutional Network (http://www.roboticsproceedings.org/rss12/p42.pdf)
+
+import numpy as np
+import struct
+import cv2 as cv
+
+class range_img_generator():
+
+    def __init__(self, h_fov, h_res, v_fov, v_res, lidar_max_range=120, lidar_min_range=0):
+
+        self.h_fov = h_fov      # LiDAR Horizontal FOV : [Min Angle, Max Angle] / Unit : Degree
+        self.h_res = h_res      # LiDAR Horizontal Angular Resolution
+
+        self.v_fov = v_fov      # LiDAR Vertical FOV : [Min Angle, Max Angle] / Unit : Degree
+        self.v_res = v_res      # LiDAR Vertical Angular Resolution
+
+        self.min_range = lidar_min_range
+        self.max_range = lidar_max_range
+
+    # Directly reading PCD binary file using numpy (Reference : KITTI Tutorial (https://github.com/windowsub0406/KITTI_Tutorial/blob/master/Convert_Velo_2_Pano_detail.ipynb))
+    def pcd_from_bin(self, bin_path):
+
+        obj = np.fromfile(bin_path, dtype=np.float32).reshape(-1, 4)
+
+        return obj[:, :3]
+
+    def convert_range_img(self, pcd_path=None, output_type='img_pixel'):
+
+        # In order to convert 3D LiDAR point cloud into 2D range image,
+        # each point in point cloud has to be converted / correponded to pixel x, y coordinates in 2D image.
+
+        # In range image, 
+        # x coordinate is mapped by horizontal elevation of 3D point
+        # y coordinate is mapped by vertical elevation of 3D point
+
+        if pcd_path == None:
+
+            print('[Error] No Data Path')
+
+            return None
+
+        else:
+
+            pcd = self.pcd_from_bin(bin_path=pcd_path)
+
+            # Resolution for final range image output
+            max_width_steps = int(np.ceil((self.h_fov[1] - self.h_fov[0]) / self.h_res))
+            max_height_steps = int(np.ceil((self.v_fov[1] - self.v_fov[0]) / self.v_res))
+
+            h_res_in_radian = self.h_res * (np.pi / 180)    # Horizontal Angular Resolution in Radian
+            v_res_in_radian = self.v_res * (np.pi / 180)    # Vertical Angular Resolution in Radian
+
+            pcd_x = pcd[:, 0]
+            pcd_y = pcd[:, 1]
+            pcd_z = pcd[:, 2]
+
+            pcd_dist_unnormalized = np.sqrt(np.power(pcd_x, 2) + np.power(pcd_y, 2) + np.power(pcd_z, 2))
+            pcd_dist_normalized = (pcd_dist_unnormalized - self.min_range) / (self.max_range - self.min_range)
+
+            x_offset = self.h_fov[0] / self.h_res   # Apply offset for negative direction angle / Avoid negative index value
+            x_in_range_img = np.arctan2(pcd_y, pcd_x) / h_res_in_radian
+            x_in_range_img = np.trunc(x_in_range_img - x_offset).astype(np.int32)
+            x_in_range_img = max(x_in_range_img) - x_in_range_img
+
+            y_offset = -1 * self.v_fov[0] / self.v_res  # Apply offset for negative direction angle / Avoid negative index value
+            y_in_range_img = np.arcsin(np.divide(pcd_z, pcd_dist_unnormalized)) / v_res_in_radian
+            y_in_range_img = np.trunc(y_in_range_img + y_offset).astype(np.int32)
+            y_in_range_img = max(y_in_range_img) - y_in_range_img
+
+            # Extra padding for range image representation in order to avoid indexing failure
+            # Due to mathematical error offsets that occur during float arithmetic operation, output size of range image varies each time.
+            # In order to avoid indexing failure from this, add padding into image.
+            img_height_padding = 3
+            img_width_padding = 3
+
+            img_height = max(y_in_range_img) + img_height_padding
+            img_width = max(x_in_range_img) + img_width_padding
+
+            range_img = np.zeros([img_height, img_width], dtype=np.uint8)
+
+            # Compose range image as the image with pixel value between 0 and 255
+            if output_type == 'img_pixel':
+                range_img[y_in_range_img, x_in_range_img] = 255 * pcd_dist_normalized
+
+                # Resize the output range image into corrected resolution
+                corrected_range_img = cv.resize(range_img, dsize=(320, 200), interpolation=cv.INTER_CUBIC)
+
+                # Add channel dimension in order to support 2D CNN layer's dimension requirements
+                corrected_range_img = np.expand_dims(corrected_range_img, axis=0)
+
+            # Compose range image as 2D array with LiDAR scanning range value between minimum and maximum scanning distance
+            elif output_type == 'depth':
+                range_img[y_in_range_img, x_in_range_img] = pcd_dist_unnormalized
+                
+                # Resize the output range image into corrected resolution
+                corrected_range_img = cv.resize(range_img, dsize=(max_width_steps, max_height_steps), interpolation=cv.INTER_CUBIC)
+                
+                # Add channel dimension in order to support 2D CNN layer's dimension requirements
+                corrected_range_img = np.expand_dims(corrected_range_img, axis=0)
+
+            return corrected_range_img
+
